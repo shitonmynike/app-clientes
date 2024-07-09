1c220eccfa3f423b82fff992fa157bf3ec0351ef
joao <74433164+shitonmynike@users.noreply.github.com>
2024-07-09 20:23:56 -0300

    Update page.tsx

--- src/app/admin/customers/page.tsx
+++ src/app/admin/customers/page.tsx
@@ -1,11 +1,12 @@
 'use client'
 import React from 'react'
+import 'react-toastify/dist/ReactToastify.css'
 import { useRouter } from 'next/navigation'
+import { ToastContainer, toast } from 'react-toastify'
 import {
   Dropdown,
   DropdownTrigger,
   DropdownMenu,
-  DropdownSection,
   DropdownItem,
   Image,
   Button,
@@ -15,37 +16,66 @@ import {
   TableColumn,
   TableRow,
   TableCell,
+  DropdownSection,
 } from '@nextui-org/react'
 import { Eye, Trash2 } from 'lucide-react'
-import { getAllCustomers } from '@/useCases/customers/getAllCustomers'
+import { getAllCustomers } from '@/app/useCases/customers/getAllCustomers'
 import { ICustomer } from '@/interfaces/customer'
+import { deleteCustomer } from '@/app/useCases/customers/deleteCustomer'
 
-export default function CostumersPage() {
+export default function CustomersPage() {
   const [listCustomers, setListCustomers] = React.useState<ICustomer[]>([])
   const router = useRouter()
 
-  React.useEffect(() => {
-    async function handleGetAllCustomers() {
+  async function handleGetAllCustomers() {
+    try {
       const response = await getAllCustomers()
       setListCustomers(response)
+    } catch (error) {
+      toast.error('Problemas com API!', {
+        theme: 'colored',
+      })
     }
+  }
+
+  async function handleDeleteCustomer(keys: React.Key) {
+    try {
+      await deleteCustomer(keys)
+      toast.success('Registro excluído', {
+        theme: 'colored',
+      })
+      handleGetAllCustomers()
+    } catch (error) {
+      toast.error('Problemas com API!', {
+        theme: 'colored',
+      })
+    }
+    console.log(keys)
+  }
+
+  /* The `React.useEffect` hook in the provided code snippet is used to perform side effects in function
+components. In this case, it is making an asynchronous call to the `getAllCustomers` function when
+the component mounts (since the dependency array `[]` is empty, indicating that the effect should
+only run once after the initial render). */
+  React.useEffect(() => {
     handleGetAllCustomers()
   }, [])
+
   return (
     <main>
       <section id="HEADER" className="flex justify-between p-6">
-        <section className="text-2x1 font-semibold">Lista dos Clientes</section>
+        <section className="text-2xl font-semibold">Lista de clientes</section>
         <section>
           <Button
             color="primary"
-            onClick={() => router.push('/admin/costumers/add')}
+            onClick={() => router.push('/admin/customers/add')}
           >
             Cadastrar Cliente
           </Button>
         </section>
       </section>
       <section id="TABLE-CUSTOMERS" className="px-6">
-        <Table aria-label="Tabel com dados de clientes">
+        <Table aria-label="Tabela com dados de clientes">
           <TableHeader>
             <TableColumn>NOME</TableColumn>
             <TableColumn>FUNÇÃO</TableColumn>
@@ -54,9 +84,11 @@ export default function CostumersPage() {
           </TableHeader>
           <TableBody
             emptyContent={
-              <section className="flex w-full flex-col items-center">
-                <Image width={300} alt="NextUI hero Image" src="/no-data.jpg" />
-                <section>Nenhum Cliente Encontrado</section>
+              <section className="mb-6 flex w-full flex-col items-center">
+                <Image width={200} alt="NextUI hero Image" src="/no-data.jpg" />
+                <section className="text-sm font-bold">
+                  Nenhum cliente encontrado
+                </section>
               </section>
             }
           >
@@ -66,6 +98,48 @@ export default function CostumersPage() {
                 <TableCell>{customer.role}</TableCell>
                 <TableCell>{customer.status}</TableCell>
                 <TableCell className="flex gap-2">
+                  <Dropdown>
+                    <DropdownTrigger>
+                      <Button
+                        isIconOnly
+                        color="danger"
+                        variant="faded"
+                        aria-label="Deletar Cliente"
+                      >
+                        <Trash2 />
+                      </Button>
+                    </DropdownTrigger>
+                    <DropdownMenu
+                      aria-label="Static Actions"
+                      onAction={(key: React.Key) => handleDeleteCustomer(key)}
+                    >
+                      <DropdownSection title="Confirma ação?" showDivider>
+                        <DropdownItem
+                          textValue="Botão excluir item"
+                          key={customer.id}
+                          shortcut=""
+                          description=""
+                          classNames={{
+                            base: 'bg-lime-500 text-white',
+                          }}
+                        >
+                          <span className="flex justify-center text-sm">
+                            Sim, excluir
+                          </span>
+                        </DropdownItem>
+                        <DropdownItem
+                          key="cancel"
+                          shortcut=""
+                          description=""
+                          textValue="Botão cancelar ação"
+                        >
+                          <span className="flex justify-center text-xs">
+                            Cancelar exclusão
+                          </span>
+                        </DropdownItem>
+                      </DropdownSection>
+                    </DropdownMenu>
+                  </Dropdown>
                   <Button
                     isIconOnly
                     color="warning"
@@ -74,20 +148,13 @@ export default function CostumersPage() {
                   >
                     <Eye />
                   </Button>
-                  <Button
-                    isIconOnly
-                    color="danger"
-                    variant="faded"
-                    aria-label="Deletar Cliente"
-                  >
-                    <Trash2 />
-                  </Button>
                 </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </section>
+      <ToastContainer />
     </main>
   )
 }
